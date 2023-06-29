951ef7a155c1c93771224cced34254b02c4d7263
Nicholas Chen <nicholaschen2003@gmail.com>
2023-06-29 11:25:31 -0500

    regressions done

--- descriptive_OLS.R
+++ descriptive_OLS.R
@@ -9,6 +9,7 @@ library(stargazer)
 library(scales)
 library(quantmod)
 library(urca)
+library(gridExtra)
 
 df <- read.csv("C:/Users/nicho/Documents/GitHub/MacroExperimentGhana/data.csv")
 df <- rename(df,
@@ -42,3 +43,45 @@ ggplot(data = plot_data, aes(x = Year, y = value, group = series),) +
   geom_line(aes(colour=series)) +
   facet_wrap(~ series, scales = "free")
 
+# M3/GDP vs log GDP Growth
+reg1 = lm(df$M3_Supply_of_GDP ~ df$log_GDP_Growth + 0)
+summary(reg1)
+beta1 = reg1$coefficients['df$log_GDP_Growth']
+plot1 <- ggplot(select(df, M3_Supply_of_GDP, log_GDP_Growth), 
+               aes(x = log_GDP_Growth, y = M3_Supply_of_GDP)) +
+  geom_line(colour='red') +
+  geom_smooth(method = "lm",
+              level = 0.99)
+
+
+# M3/GDP vs log GDP/Worker
+reg2 = lm(df$M3_Supply_of_GDP ~ df$log_GDP_per_Worker + 0)
+summary(reg2)
+beta2 = reg2$coefficients['df$log_GDP_per_Worker']
+plot2 <- ggplot(select(df, M3_Supply_of_GDP, log_GDP_per_Worker), 
+               aes(x = log_GDP_per_Worker, y = M3_Supply_of_GDP)) +
+  geom_line(colour='green') +
+  geom_smooth(method = "lm",
+              level = 0.99)
+
+# M3/GDP vs log Labor Force
+reg3 = lm(df$M3_Supply_of_GDP ~ df$log_Labor_Force + 0)
+summary(reg3)
+beta3 = reg3$coefficients['df$log_Labor_Force']
+plot3 <- ggplot(select(df, M3_Supply_of_GDP, log_Labor_Force), 
+               aes(x = log_Labor_Force, y = M3_Supply_of_GDP)) +
+  geom_line(colour='purple') +
+  geom_smooth(method = "lm",
+              level = 0.99)
+
+# M3/GDP vs log CPI
+reg4 = lm(df$M3_Supply_of_GDP ~ df$log_CPI + 0)
+summary(reg4)
+beta4 = reg4$coefficients['df$log_CPI']
+plot4 <- ggplot(select(df, M3_Supply_of_GDP, log_CPI), 
+               aes(x = log_CPI, y = M3_Supply_of_GDP)) +
+  geom_line(colour='orange') +
+  geom_smooth(method = "lm",
+              level = 0.99)
+
+grid.arrange(`plot1`, `plot2`, `plot3`, `plot4`, nrow=2)
